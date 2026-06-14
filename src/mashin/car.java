15d21fd78138cab2154661738674a3963d3b5b5a
sogolf24 <ghorbani.sogoli@gmail.com>
2026-06-14 17:03:19 +0200

    First

---
+++ src/mashin/car.java
@@ -0,0 +1,13 @@
+package mashin;
+
+public class car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//maraz simulatrasdasdasd
+		//moz
+		// manam manam madaretoon
+	}
+
+}
